7d57df5eb44ad2adfb056d031642023c4e19fc89
魏雨潇 <wyxxiao@outlook.com>
2020-03-12 16:29:54 +0800

    20200312

---
+++ ProjectHelloworld/homeOutput.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ProjectHelloworld
+{
+    public partial class homeOutput : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.Title = "这是我的第二个动态网页";
+            Response.Write("<script language='javascript'>alert('欢迎学习《数据思维下的算法与程序设计》课程')</script>");
+        }
+    }
+}
